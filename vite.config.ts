c3ae2f3ecf35bea93238dd3de65b8f4d501224fa
ashinwz <297787884@qq.com>
2023-03-12 18:46:04 +0800

    remove loadEnv

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import path from 'path'
-import { defineConfig, loadEnv } from 'vite'
+// import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { VitePWA } from 'vite-plugin-pwa'
 
